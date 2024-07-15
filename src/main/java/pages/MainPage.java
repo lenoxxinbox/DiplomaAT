d6a38c83da232b140852b4d54fa02277ab3d6be4
iuliia sinkova <juliasinkova97@gmail.com>
2024-07-15 16:55:59 +0400

    And config.properties, MainPage, fullAuthorizationMethod

---
+++ src/main/java/pages/MainPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage {
+    private final static SelenideElement USERS_LINK = $x("//a[text()='Users']");
+    private final static SelenideElement CREATE_NEW_USER_LINK = $x("//a[@href='#/create/user']");
+
+    public static void openCreateNewUserPage() {
+        USERS_LINK.click();
+        CREATE_NEW_USER_LINK.shouldBe(visible);
+        CREATE_NEW_USER_LINK.click();
+    }
+}
