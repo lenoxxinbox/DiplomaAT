1ea0140e022607571f1abf717ff76fd849a569fa
OUT-Dmitrienko.E.V <OUT-Dmitrienko.E.V@sbertech.ru>
2024-07-15 09:17:13 +0300

    Добавила тесты BuySellCar

---
+++ src/test/java/ui/BuySellCarTest.java
@@ -0,0 +1,80 @@
+package ui;
+
+import base.BaseTest;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BuySellCarTest extends BaseTest {
+
+    @BeforeEach
+    public void inTest() {
+        Configuration.timeout = 50000;
+        loginPage.isPageOpen().login();
+        Selenide.switchTo().alert().accept();
+    }
+
+    @Test
+    @DisplayName("Успешная покупка автомобиля")
+    @Owner("Elena Dmitrienko")
+    public void buyCar() {
+        Configuration.timeout = 50000;
+        String expectedMessage = "Status: Successfully pushed, code: 200";
+        String actualMessage = buySellCarPage.goToBuySellCarMenu().
+                userIDInput("14").
+                carIDInput("54").
+                buy().
+                push().
+                getResultMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("Успешная продажа автомобиля")
+    @Owner("Elena Dmitrienko")
+    public void sellCar() {
+        Configuration.timeout = 50000;
+        String expectedMessage = "Status: Successfully pushed, code: 200";
+        String actualMessage = buySellCarPage.goToBuySellCarMenu().
+                userIDInput("14").
+                carIDInput("54").
+                sell().
+                push().
+                getResultMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("Продажа несуществующего автомобиля")
+    @Owner("Elena Dmitrienko")
+    public void sellNotCar() {
+        Configuration.timeout = 50000;
+        String expectedMessage = "Status: AxiosError: Request failed with status code 404";
+        String actualMessage = buySellCarPage.goToBuySellCarMenu().
+                userIDInput("14").
+                carIDInput("101010101010").
+                sell().
+                push().
+                getResultMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("Продажа автомобиля несуществующим пользователем")
+    @Owner("Elena Dmitrienko")
+    public void buyNotUser() {
+        Configuration.timeout = 50000;
+        String expectedMessage = "Status: AxiosError: Request failed with status code 404";
+        String actualMessage = buySellCarPage.goToBuySellCarMenu().
+                userIDInput("101010101010").
+                carIDInput("54").
+                buy().
+                push().
+                getResultMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+}
