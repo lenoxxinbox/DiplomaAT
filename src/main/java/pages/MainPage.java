14869f2bde49e792806059884a43c9319685f049
mr-Yellowman <166898323+mr-Yellowman@users.noreply.github.com>
2024-07-22 09:44:29 +0400

    Update MainPage.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -8,10 +8,16 @@ import static com.codeborne.selenide.Selenide.$x;
 public class MainPage {
     private final static SelenideElement USERS_LINK = $x("//a[text()='Users']");
     private final static SelenideElement CREATE_NEW_USER_LINK = $x("//a[@href='#/create/user']");
+    private final static SelenideElement HOUSES_MENU = $x("//a[contains(text(), 'Houses')]");
+    private final static SelenideElement HOUSES_MENU_CREATE_NEW = $x("//a[contains(text(), 'Create new')]");
 
     public static void openCreateNewUserPage() {
         USERS_LINK.click();
         CREATE_NEW_USER_LINK.shouldBe(visible);
         CREATE_NEW_USER_LINK.click();
+    }
+    public static void goCreateHousePage() {
+        HOUSES_MENU.click();
+        HOUSES_MENU_CREATE_NEW.click();
     }
 }
